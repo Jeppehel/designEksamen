dc5bdb12a732faf23cb9a01f9a54ac131dfb2764
casper Nielsen <casper_1125@hotmail.com>
2018-11-28 10:27:42 +0100

    mockup tilføjet

---
+++ MockUpEksamenS3/src/test/java/swc3/eksamen/mockup/MockupApplicationTests.java
@@ -0,0 +1,16 @@
+package swc3.eksamen.mockup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockupApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
