36ce844dbaede378d4fa853f64434e82b30f77cf
Christian Bjørk Christiansen <chri279v@stud.kea.dk>
2018-11-29 10:31:59 +0100

    dataconfig

---
+++ 3.SemesterDesignProjekt/src/main/java/com/example/demo/configs/DatabaseConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DriverManagerDataSource dataSource(){
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://sphdb.cg1jcfnkmz1g.eu-central-1.rds.amazonaws.com/db?useSSL=false");
+        driverManagerDataSource.setUsername("sph_design");
+        driverManagerDataSource.setPassword("{P4h!g8aK_bjx%");
+
+        return driverManagerDataSource;
+    }
+
+}
