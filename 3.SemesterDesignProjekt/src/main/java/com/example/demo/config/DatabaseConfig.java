ac216b853053af2ccd049e613ed6fbeb5dcddd33
casper Nielsen <casper_1125@hotmail.com>
2018-12-06 08:52:51 +0100

    opdatering

--- 3.SemesterDesignProjekt/src/main/java/com/example/demo/config/DatabaseConfig.java
+++ 3.SemesterDesignProjekt/src/main/java/com/example/demo/config/DatabaseConfig.java
@@ -6,21 +6,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 
-
-@Configuration
-public class DatabaseConfig {
-
-    @Bean (name = "dataSource")
-    public DriverManagerDataSource dataSource(){
-
-        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-
-        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        driverManagerDataSource.setUrl("jdbc:mysql://sphdb.cg1jcfnkmz1g.eu-central-1.rds.amazonaws.com/db?useSSL=false");
-        driverManagerDataSource.setUsername("sph_design");
-        driverManagerDataSource.setPassword("{P4h!g8aK_bjx%");
-
-        return driverManagerDataSource;
-    }
-
-}
+//
+//@Configuration
+//public class DatabaseConfig {
+//
+//    @Bean (name = "dataSource")
+//    public DriverManagerDataSource dataSource(){
+//
+//        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+//
+//        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        driverManagerDataSource.setUrl("jdbc:mysql://sphdb.cg1jcfnkmz1g.eu-central-1.rds.amazonaws.com/db?useSSL=false");
+//        driverManagerDataSource.setUsername("sph_design");
+//        driverManagerDataSource.setPassword("{P4h!g8aK_bjx%");
+//
+//        return driverManagerDataSource;
+//    }
+//
+//}
