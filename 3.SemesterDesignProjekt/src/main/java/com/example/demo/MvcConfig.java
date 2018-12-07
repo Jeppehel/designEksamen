8c07f78a873979cf83d1e4a3b9cf4e3fdc2f4517
casper Nielsen <casper_1125@hotmail.com>
2018-12-07 11:19:29 +0100

    safety update

--- 3.SemesterDesignProjekt/src/main/java/com/example/demo/MvcConfig.java
+++ 3.SemesterDesignProjekt/src/main/java/com/example/demo/MvcConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class MvcConfig implements WebMvcConfigurer {
-//
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/index").setViewName("Index");
-//        registry.addViewController("/").setViewName("Index");
-//    }
-//
-//}
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("Index");
+        registry.addViewController("/").setViewName("Index");
+    }
+
+}
--- 3.SemesterDesignProjekt/src/main/java/com/example/demo/config/DatabaseConfig.java
+++ 3.SemesterDesignProjekt/src/main/java/com/example/demo/config/DatabaseConfig.java
@@ -5,22 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-//
-//
-//@Configuration
-//public class DatabaseConfig {
-//
-//    @Bean (name = "dataSource")
-//    public DriverManagerDataSource dataSource(){
-//
-//        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-//
-//        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//        driverManagerDataSource.setUrl("jdbc:mysql://sphdb.cg1jcfnkmz1g.eu-central-1.rds.amazonaws.com/db?useSSL=false");
-//        driverManagerDataSource.setUsername("sph_design");
-//        driverManagerDataSource.setPassword("{P4h!g8aK_bjx%");
-//
-//        return driverManagerDataSource;
-//    }
-//
-//}
+
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean (name = "dataSource")
+    public DriverManagerDataSource dataSource(){
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://sph.cg1jcfnkmz1g.eu-central-1.rds.amazonaws.com");
+        driverManagerDataSource.setUsername("sph_db");
+        driverManagerDataSource.setPassword("{df3!D8nd%Am");
+
+        return driverManagerDataSource;
+    }
+
+}
