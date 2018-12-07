b2f2cc8d14a1238b00e329430ffab2a98fed07e7
Christian Bjørk Christiansen <chri279v@stud.kea.dk>
2018-12-07 11:17:02 +0100

    update

--- 3.SemesterDesignProjekt/src/main/java/com/example/demo/controller/AdminController.java
+++ 3.SemesterDesignProjekt/src/main/java/com/example/demo/controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class AdminController {
 
-    @RequestMapping(value="/login")
+    @GetMapping(value = "/login")
     public String login(){
         return "login";
     }
