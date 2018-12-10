826b7b7e8ec29efddc882db5d4eaf957f0edc591
Christian Bjørk Christiansen <chri279v@stud.kea.dk>
2018-12-10 12:39:09 +0100

    article rep, service, service impl

--- 3.SemesterDesignProjekt/src/main/java/com/example/demo/domain/Article.java
+++ 3.SemesterDesignProjekt/src/main/java/com/example/demo/domain/Article.java
@@ -1,4 +1,87 @@
 package com.example.demo.domain;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Blob;
+import java.util.Date;
+
+@Entity
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date date;
+    private String title;
+    private String description;
+    //Binary Large Objects" and are used to store large amounts of binary data, such as images
+    private Blob image;
+
+
+    public Article() {
+    }
+
+    public Article(Date date, String title, String description, Blob image) {
+        this.date = date;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Blob getImage() {
+        return image;
+    }
+
+    public void setImage(Blob image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", date=" + date +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", image=" + image +
+                '}';
+    }
 }
+
