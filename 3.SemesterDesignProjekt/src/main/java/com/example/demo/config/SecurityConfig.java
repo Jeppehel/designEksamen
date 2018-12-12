262ec867924f465e5ff9c96c6867193fe395a280
Unknown <andreassharteg@gmail.com>
2018-12-12 12:41:24 +0100

    permit css i security

--- 3.SemesterDesignProjekt/src/main/java/com/example/demo/config/SecurityConfig.java
+++ 3.SemesterDesignProjekt/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/logo.png", "/Kanal_css.css","/nav-script.js","/nav_css.css").permitAll()
                 .antMatchers("/admin", "/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
                 .permitAll();
